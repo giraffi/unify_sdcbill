218638e0d5eaa94020430af2b44a72a2ec88a052
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-05 12:56:56 +0900

    1st runnable version.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "redis"
+gem "pry", :group => :development
+gem "pry-doc", :group => :development
---
+++ unite.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+#  coding: utf-8
+require 'bundler/setup'
+require 'csv'
+require 'json'
+require 'redis'
+
+# configure
+redis_db = 2
+
+
+rfile = "result/all_result.csv"
+
+kvs = Redis.new(:db => redis_db)
+kvs.flushdb
+
+datafiles = Dir.glob("./sampledata/*.json")
+
+datafiles.each do |datafile|
+  billing_by_owners = JSON.load(File.read(datafile))
+  billing_by_owners.map do |owner, machines|
+    machines.each do |machine, machine_data|
+      machine_data["metering"]["network"].keys.each do |netx|
+        %w(bytes_sent_delta bytes_received_delta).each do |w|
+          kvs.hincrby(owner, "#{machine}/#{netx}/#{w}", machine_data["metering"]["network"][netx][w])
+        end
+      end
+    end
+  end
+end  
+
+
+csvh = ["owner_uuid", "zone_uuid","net_if","Megabytes_sent_delta","Megabytes_received_delta"]
+owner_ids = kvs.keys("????????-????-????-????-????????????")
+
+CSV.open(rfile, "wb",:force_quotes => true) do |writer|
+  writer << csvh
+  owner_ids.each do |k_owner|
+    kvs.hgetall(k_owner).each do |machine_nif, meter_count|
+      zone_uuid, zone_nif, net_vector = machine_nif.split("/")
+      next if net_vector == "bytes_received_delta"
+
+      sent_delta = kvs.hget(k_owner, "#{zone_uuid}/#{zone_nif}/bytes_sent_delta").to_i / 1024 / 1024
+      received_delta = kvs.hget(k_owner, "#{zone_uuid}/#{zone_nif}/bytes_received_delta").to_i / 1024 / 1024
+      writer << [k_owner, zone_uuid, zone_nif, sent_delta, received_delta]
+    end
+  end
+end
